884ac45320c963bc93fa6ac94f0df0db9c8d6dca
Darriel Markerizal <darrielmarkerizal@gmail.com>
2023-11-09 08:33:48 +0700

    add data class

--- app/src/main/java/com/example/pertemuan11/model/Users.kt
+++ app/src/main/java/com/example/pertemuan11/model/Users.kt
@@ -2,7 +2,7 @@ package com.example.pertemuan11.model
 
 import com.google.gson.annotations.SerializedName
 
-data class Data(
-    @SerializedName("employee_name")
-    var employeeName : String,
+data class Users(
+    @SerializedName("data")
+    var data: List<Data>
 )
